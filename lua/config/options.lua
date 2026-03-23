4bc191932217d1a08819e1efbcd7127e2d9d86ee
ik0326 <ik0246game0246@gmail.com>
2026-03-23 22:57:48 +0900

    不要なアニメーションなどをデフォルト無効化

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,3 +1,10 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+vim.g.lazyvim_inlay_hints = false
+
+-- スペルチェックを有効にする
+vim.opt.spell = true
+-- 対象言語を英語（en）と、必要に応じて日本語（cjk）に設定
+-- cjkを入れておくと日本語文字がスペルミスとして判定されにくくなります
+vim.opt.spelllang = { "en_us" }
---
+++ lua/plugins/ui.lua
@@ -0,0 +1,13 @@
+return {
+  {
+    "folke/snacks.nvim",
+    opts = {
+      scroll = { enabled = false },
+    },
+  },
+  -- mini.animate（その他の各種アニメーション）を完全に無効化
+  {
+    "echasnovski/mini.animate",
+    enabled = false,
+  },
+}
